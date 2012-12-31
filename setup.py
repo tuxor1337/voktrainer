32d8c76e4e51e4fce552684ed4595e446232f41e
Thomas Vogt <tuxor1337@web.de>
2012-12-31 17:20:19 +0100

    Added setup script

---
+++ setup.py
@@ -0,0 +1,31 @@
+# -*- coding: utf-8 -*-
+
+from distutils.core import setup
+from distutils.command.build_scripts \
+      import build_scripts as distutils_build_scripts
+
+class build_scripts(distutils_build_scripts):
+    description = "copy scripts to build directory"
+
+    def run(self):
+        self.mkpath(self.build_dir)
+        for script in self.scripts:
+            newpath = os.path.join(self.build_dir, os.path.basename(script))
+            if newpath.lower().endswith(".py"):
+                newpath = newpath[:-3]
+            if newer(script, newpath) or self.force:
+                self.copy_file(script, newpath)
+
+setup(name='Vokabeltrainer für Linux',
+      version='1.0',
+      cmd_classes={"build_scripts": build_scripts},
+      description='Vokabeltrainer geschrieben in Python',
+      author='Thomas Vogt',
+      author_email='tuxor1337@web.de',
+      url='https://github.com/tuxor1337/voktrainer',
+      packages=['vok'],
+      scripts=["voktrainer.py"],
+      data_files=[("share/applications",["setup_data/voktrainer.desktop"]),
+                  ("share/pixmaps",["setup_data/voktrainer.svg"])]
+     )
+
